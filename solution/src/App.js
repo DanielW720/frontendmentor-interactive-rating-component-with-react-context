0f16695cd9e9f10fd12dc136fa1bd2d578e0ea98
DW Desktop <westerlund.da@gmail.com>
2023-03-29 20:41:51 +0200

    Changed from useState to useReducer for rating

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,26 +1,66 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import Rating from './components/feedback/Rating';
 import ThankYou from './components/feedback/ThankYou';
 
+function ratingReducer(ratingState, action) {
+  switch (action.type) {
+    case 'chose-rating': {
+      return action.rating;
+    }
+    case 'reset': {
+      return null;
+    }
+    default: {
+      throw Error('Unkown action: ' + action.type);
+    }
+  }
+}
+
 function App() {
-  const [rating, setRating] = useState('');
+  // const [rating, setRating] = useState('');
+  const [rating, dispatch] = useReducer(ratingReducer, 0);
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const ratingHandler = (chosenRating) => {
-    setRating(chosenRating);
-  }
+  // const ratingHandler = (chosenRating) => {
+  //   setRating(chosenRating);
+  // }
 
   const submitHandler = () => {
     if (Number.isInteger(rating)) {
       setIsSubmitted(true);
     } else {
-      console.log("Please choose a rating before submitting!")
+      console.log("Please choose a rating before submitting!" + rating);
     }
   }
 
+  // const reset = () => {
+  //   setRating('');
+  //   setIsSubmitted(false);
+  // }
+
+  const ratingHandler = (chosenRating) => {
+    dispatch({
+      type: 'chose-rating',
+      rating: chosenRating
+    });
+  }
+
+  // const submitHandler = () => {
+  //   if (Number.isInteger(rating)) {
+  //     dispatch({
+  //       type: 'submitted',
+  //     });
+  //   } else {
+  //     // TODO: Present this to user
+  //     console.log("Please choose a rating before submitting!")
+  //   }
+  // }
+
   const reset = () => {
-    setRating('');
+    dispatch({
+      type: 'reset'
+    });
     setIsSubmitted(false);
   }
 
